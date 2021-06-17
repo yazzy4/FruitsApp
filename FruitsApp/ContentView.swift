7fb37c51b1eae019ea7af1b4c3de8cf4c9eee861
Yaz Burrell <yburrell@zillion.com>
2021-06-17 14:11:36 -0400

    first commit

---
+++ FruitsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FruitsApp
+//
+//  Created by Yaz Burrell on 6/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
